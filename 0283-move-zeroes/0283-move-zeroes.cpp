8d87cc3896da67b5ffe8404eb10be47c1c0edb33
SHIV SINGH <shivsinghrajput348@gmail.com>
2026-07-26 15:34:27 +0530

    Time: 0 ms (100%), Space: 23.8 MB (83.37%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,20 +4,21 @@
 public:
 
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        int temp[n];
-        int j=0;
-        for(int i=0; i<n; i++){
-            if(nums[i] != 0){
-                temp[j]=nums[i];
-                j++;
-            }
-        } while(j<n){
-            temp[j]=0;
+       int n = nums.size();
+       int temp[n];
+       int j=0;
+       for(int i = 0; i<n; i++){
+        if(nums[i] != 0){
+            temp[j] = nums[i];
             j++;
+        } }
+        for( int i=j; i<n; i++){
+            temp[i] = 0;
         }
         for(int i=0; i<n; i++){
-            nums[i]=temp[i];
+            nums[i] = temp[i];
         }
-    }
+        return ;
+        } 
+    
 };
